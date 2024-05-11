e6c19db77076ab67f782b0feb4a2d3db24581a50
airvt1x <airat3552@gmail.com>
2024-05-11 13:17:42 +0300

    niggas

---
+++ back/models/Note.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+
+const NoteSchema = new mongoose.Schema({
+    note_data: {
+        type: Map,
+        required: true,
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    priority: {
+        type: Array,
+    },
+    favorite:{
+        type: Boolean
+    },
+    },
+    {
+        timestamps:true,
+    }
+);
+
+export default mongoose.model('Note', NoteSchema);
